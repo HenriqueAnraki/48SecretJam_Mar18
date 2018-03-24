459f20fd149dbf37c4ffff589719c3ff4f505ab3
HenriqueAnraki <henrique_anraki@outlook.com>
2018-03-24 07:10:17 -0300

    movimento basico + inicio interacao com objetos + exclusao da pasta Library

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+	// Use this for initialization
+	private float kScale;
+	private Vector3 normalScale;
+	void Start () {
+		kScale = 1.2f;
+		normalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            transform.localScale = new Vector3(transform.localScale.x * kScale, transform.localScale.y * kScale, transform.localScale.z);
+        }
+    }
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+            transform.localScale = normalScale;
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody2D Rigid;
+	public float speed;
+
+	void Start () {
+		Rigid = gameObject.GetComponent<Rigidbody2D>();
+		
+		speed = 3;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move() {
+		//transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.y);
+
+		float h = Input.GetAxisRaw ("Horizontal");
+		//float v = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (h, 0.0f);
+
+		Rigid.velocity = movement.normalized * speed;		
+		
+	}
+}
